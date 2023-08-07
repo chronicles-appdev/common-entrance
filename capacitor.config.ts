7cca758f6b55585b9fe9d3e520a3f689412ad47d
Aliu Adedigba <indexcorpng@gmail.com>
2023-08-07 11:35:03 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'common-entrance',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
